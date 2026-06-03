23ef64187db3f4240f1b4d6097078dbc7a92182f
Alexandr <alexyeryomin13@gmail.com>
2026-06-03 15:24:33 +0300

    "chore: add Supabase client and providers setup"

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-
+import { supabase } from './lib/supabase'
+console.log(supabase)
 function App() {
   const [count, setCount] = useState(0)
 
